51c03be9b20a5a36e849d866f315afda2c296620
Ricky Reusser <rsreusser@gmail.com>
2017-04-14 14:02:18 -0400

    😑

--- examples/basic/.idyll/components.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-"header": require('/Users/rreusser/.nvm/versions/node/v6.8.0/lib/node_modules/idyll/src/components/header'),
-"equation": require('/Users/rreusser/.nvm/versions/node/v6.8.0/lib/node_modules/idyll/src/components/equation'),
-"lineplot": require('/Users/rreusser/projects/idyll/idyll-loader-component/examples/basic/components/lineplot')
-} 
